9ca149f34825b463b183109833d524e73ed81fea
TalhaProgrammer92 <talha.code92@gmail.com>
2025-10-30 18:52:39 +0500

    Add IOrderRepository interface and OrderRepository implementation

---
+++ PizzaStore/PizzaStore.ApplicationCore/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using PizzaStore.Domain.Entities.Order;
+
+namespace PizzaStore.ApplicationCore.Interfaces.Repositories
+{
+    public interface IOrderRepository : IGeneralRepository<Order>
+    {
+    }
+}
---
+++ PizzaStore/PizzaStore.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using PizzaStore.ApplicationCore.Interfaces.Repositories;
+using PizzaStore.Domain.Entities.Order;
+using PizzaStore.Infrastructure.Data;
+
+namespace PizzaStore.Infrastructure.Repositories
+{
+    public class OrderRepository : GeneralRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(PizzaStoreDbContext context) : base(context)
+        {
+        }
+    }
+}
